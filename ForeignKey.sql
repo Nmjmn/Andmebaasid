1f93431b9434c1470cdc8f4f6d13ba8735a8ebf2
Nmjmn <nmjmneue@gmail.com>
2026-04-14 15:46:49 +0300

    Update ForeignKey.sql

--- ForeignKey.sql
+++ ForeignKey.sql
@@ -22,3 +22,24 @@ Drop Database ...;
 --tabelirida kustutamine
 select * from opilane;
 delete from opilane where opilaneID=3;
+
+--kolmas tabel
+CREATE TABLE opetaja(
+opetajaID int Primary Key identity(1, 1),
+eesnimi varchar(25),
+ruum int not null,)
+INSERT INTO opetaja
+VALUES ('Nimi', 24) 
+--neljas tabel
+CREATE TABLE opetamine(
+opetamineID int Primary Key identity(1, 1),
+opetajaID int,
+Foreign Key (opetajaID) References opetaja(opetajaID),
+oppeaine varchar(25),
+maht int not null)
+INSERT INTO opetamine
+VALUES (1, 'keemia', 24)
+
+
+Select * from opetaja;
+Select * from opetamine;
