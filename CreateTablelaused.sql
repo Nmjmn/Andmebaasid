d02fb520d0df7745e567c67a8cf01dd8529d3f4d
Nmjmn <nmjmneue@gmail.com>
2026-04-15 12:41:41 +0300

    Update CreateTablelaused.sql

--- CreateTablelaused.sql
+++ CreateTablelaused.sql
@@ -12,56 +12,6 @@ keskmineHinne decimal(2, 1))
 
 SELECT * FROM opilane;
 
---uue veeru lisamine
-alter table opilane add telefon varchar(1);
-
-
---tabeli struktuuri muutmine -veeru kustatamine
-alter table opilane drop column telefon ;
-
---tabeli muutmine ´- veeru andemtüübi vahetamine
-alter table opilane alter column  telefon varchar(13);
---store procedure help, mis näitab tabeli struktuuri ja veerude andmetüübid.
-sp_help opilane;
-
-drop table ryhm;
-
-create table ryhm(
-ryhmID int not null, 
-ryhmNIMI varchar(10));
-
-Select * from ryhm;
---muudame tabeli ryhm ja lisame Primary key
-Alter Table ryhm Add constraint pk_ryhm Primary key (ryhmID);
-
-
---muudame tabeli ryhm ja  teeme ryhmNimi unikaalseks UNIQUE
-Alter Table ryhm Add constraint un_ryhm UNIQUE(ryhmNimi);
-
-sp_help ryhm;
-
---täidame tabeli ryhm
-
-insert into ryhm(ryhmID, ryhmNIMI)
-Values (3, 'LOGITPE24'),(4, 'TITpe24');
-select * from ryhm;
-
---FOREIGN KEY -- opilane tabelis
---lisame opilane tabelisse uus veerg
-
-alter table opilane add ryhmID int;
-select * from opilane
-select * from ryhm
-
---lisame ryhmID foreign key
-
-alter table opilane add constraint fk_ryhm
-Foreign key (ryhmID) References ryhm(ryhmID) --link tabel(PK veerg)
-
---kontrollimiseks täidame tabeli opilane
-INSERT INTO opilane
-VALUES ('Kevin', 'Allik', '2008-10-23', 0, 'Tartu,Eesti', 4.5, '+45555', 2)
-
 --admete lisamine tabelisse
 INSERT INTO opilane
 VALUES ('Valge', 'Sall', '2000-10-23'),
